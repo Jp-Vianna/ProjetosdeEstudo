07427257fd8c2152ced9dc1a94149f58be533653
Jp-Vianna <142637763+Jp-Vianna@users.noreply.github.com>
2023-12-10 19:57:28 -0300

    Update GrafoErdosRenyi.java
    Adiciona comentários explicando a execução dos testes.

--- Trab. E.D - ErdosReyni -/src/GrafoErdosRenyi.java
+++ Trab. E.D - ErdosReyni -/src/GrafoErdosRenyi.java
@@ -1,23 +1,23 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Random;
+import java.io.BufferedWriter; //Import para escrever os resultados no arquivo .txt
+import java.io.FileWriter;     //Import para escrever os resultados no arquivo .txt
+import java.io.IOException;    //Excecao de entrada e saida
+import java.io.PrintWriter;    //Import para escrever os resultados no arquivo .txt
+import java.util.Random;       //Gerar valores aleatorios
 
 public class GrafoErdosRenyi {
     //gerarGrafo e Main ok
     public static void gerarGrafo(ordenacaoTopologica grafo, int numNo, double probabilidade) {
-        Random random = new Random();
+        Random random = new Random(); //Gera valores aleatorios
 
-        for (int i = 1; i <= numNo; i++){
-            for (int j = 1; j <= numNo; j++){
-                if(i != j){
-                    if (random.nextDouble() < probabilidade){
-                        grafo.adicionarAresta(i, j);
+        for (int i = 1; i <= numNo; i++){     //For percorrendo a "chave" dos vertices
+            for (int j = 1; j <= numNo; j++){     //For percorrendo a "chave" dos vertices
+                if(i != j){     //Impede de um vertice criar aresta para si mesmo
+                    if (random.nextDouble() < probabilidade){     //Determina se a aresta sera criada ou nao
+                        grafo.adicionarAresta(i, j);     //Aresta entre i e j
 
-                        if(grafo.temCiclo(numNo)){
-                            System.out.println("A aresta " + i + " e " + j + " causou um ciclo.");
-                            grafo.removeAresta(i, j);
+                        if(grafo.temCiclo(numNo)){     //Verifica se gerou um ciclo
+                            System.out.println("A aresta " + i + " e " + j + " causou um ciclo.");     //Informa o ciclo
+                            grafo.removeAresta(i, j);     //Remove aresta criada
                         }
                     }
                 }
@@ -26,37 +26,38 @@ public class GrafoErdosRenyi {
     }
 
     public static void main(String[] args) throws IOException {
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("saida.txt", true)));
-        int numNo = 20;//V: 10, 20, 30, 40, 50, 100, 200, 500, 1.000, 5.000, 10.000, 20.000, 30.000, 50.000 e 100.000
-        double probabilidade = 0.15;
-        int qtd = 10;
-        ordenacaoTopologica grafo = new ordenacaoTopologica();
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("saida.txt", true)));     //Instancia objeto de escrita
+        
+        int numNo = 20000;     //Estabelece o tamanho do grafo a ser criado        
+        double probabilidade = 0.15;     //Estabelece uma probabilidade de 15% de criar a aresta
+        int qtd = 10;     //Quantidade de testes a serem executados
+        ordenacaoTopologica grafo = new ordenacaoTopologica();     //Instancia grafo
 
-        long t0 = System.currentTimeMillis();
+        long t0 = System.currentTimeMillis();     //Realiza marcacao do tempo de execucao
 
-        for(int i = 0; i < qtd; i++){
-            grafo.limpaGrafo();
+        for(int i = 0; i < qtd; i++){     //Roda os testes
+            grafo.limpaGrafo();     //Limpa o grafo
 
-            gerarGrafo(grafo, numNo, probabilidade);
+            gerarGrafo(grafo, numNo, probabilidade);     //Gera um grafo aleatorio
 
-            if (!grafo.executa())
+            if (!grafo.executa())     //Realiza ordenacao topologica do grafo gerado
                 System.out.println("  //O conjunto nao eh parcialmente ordenado.");
             else
                 System.out.println("  //O conjunto eh parcialmente ordenado.");
 
-            boolean possuiCiclos = grafo.temCiclo(numNo);
+            boolean possuiCiclos = grafo.temCiclo(numNo);     //Verifica a existencia de ciclos por garantia
 
-            System.out.println("\nPossui ciclos: " + possuiCiclos);
+            System.out.println("\nPossui ciclos: " + possuiCiclos);     //Mostra resultado
         }
 
-        long t1 = System.currentTimeMillis();
-        long tempoProcessamentoTotal = t1 - t0;
+        long t1 = System.currentTimeMillis();     //Finaliza medicao de tempo
+        long tempoProcessamentoTotal = t1 - t0;     //Calcula o tempo total
 
-        System.out.println("Tempo total: " + tempoProcessamentoTotal);
-        String linha = "Vezes rodadas: " + qtd + " Tamanho: " + numNo + " Tempo: " + tempoProcessamentoTotal;
+        System.out.println("Tempo total: " + tempoProcessamentoTotal);     //Mostra resultado tempo
+        String linha = "Vezes rodadas: " + qtd + " Tamanho: " + numNo + " Tempo: " + tempoProcessamentoTotal;     //Linha com os resultados a ser escrita
 
-        out.println(linha);
-        out.println();
-        out.close();
+        out.println(linha);     //Escreve linha no .txt de saida
+        out.println();     //Quebra linha 
+        out.close();     //Fecha buffer
     }
 }
